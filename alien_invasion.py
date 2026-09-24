3fe2e8c1e96ac2ba44fc63a01a60b6015ac5ec24
Kazuma Shikata <kazuma.shikata@student.uts.edu.au>
2026-09-25 07:13:48 +1000

    add collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -75,6 +75,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        #Check for any bullets that have hit aliens
+        # If so, get rid of the bullet and the alien.
+        collisons = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
