f51386eabdffcfe0b27bebe4b0a658e18bffbbbb
Kazuma Shikata <kazuma.shikata@student.uts.edu.au>
2026-09-25 14:45:16 +1000

    Destroy existing bullets and create new fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,10 @@ class AlienInvasion:
         # If so, get rid of the bullet and the alien.
         collisons = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            #Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
